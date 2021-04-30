33c09068778ed3b67d30b3b579044fa15f0ee881
brettfelmate <the.bretter.email@gmail.com>
2021-04-30 13:21:47 -0300

    First commit

---
+++ experiment.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Brett Feltmate"
+
+import klibs
+from klibs import P
+
+class CompensatoryTrackingTask(klibs.Experiment):
+
+	def setup(self):
+		pass
+
+	def block(self):
+		pass
+
+	def setup_response_collector(self):
+		pass
+
+	def trial_prep(self):
+		pass
+
+	def trial(self):
+
+		return {
+			"block_num": P.block_number,
+			"trial_num": P.trial_number
+		}
+
+	def trial_clean_up(self):
+		pass
+
+	def clean_up(self):
+		pass
